cb0553fd608a757a7d86437305572a7c1107e1be
Yuval Kohavi <yuval.kohavi@gmail.com>
2017-05-25 19:54:16 -0400

    wifi works

--- connectwififirmware.ino
+++ connectwififirmware.ino
@@ -19,6 +19,11 @@ const IPAddress broadcast_udp(255,255,255,255);   // remote IP of the target dev
 const unsigned int destPort = 3134;    // remote port of the target device where the NodeMCU sends OSC to
 
 
+byte last_packet[PACKET_SIZE] = {0};
+
+unsigned long send_deadline = 0;
+constexpr unsigned long SEND_PERIOD = 5000;
+
 // the setup routine runs once when you press reset:
 void setup() {                
   Serial.begin(9600);
@@ -28,31 +33,37 @@ void setup() {
   WiFiManager wifiManager;
   //first parameter is name of access point, second is the password
   wifiManager.autoConnect("Connect_FW_WiFi", "Connect_FW_WiFi");
+
+  send_deadline = millis() + SEND_PERIOD;
 }
+
 
 // the loop routine runs over and over again forever:
 void loop() {
+  static bool pinstate = false;
   byte buffer_datagram[PACKET_SIZE];
   if (Serial.available())
   {
+    pinstate = !pinstate;
+        digitalWrite(led, pinstate?HIGH:LOW);
     Serial.readBytes(buffer_datagram, PACKET_SIZE);
       if (checkCRC(buffer_datagram))
       {
-        OSCMessage msgOut("/pole_touch");
-        uint8_t * blob = buffer_datagram;
-        msgOut.add(blob, PACKET_SIZE);
-        
-        Udp.beginPacket(broadcast_udp,port_udp);
-        msgOut.send(Udp);
-        Udp.endPacket();
-        msgOut.empty();
+        memcpy(last_packet, buffer_datagram, PACKET_SIZE);
+        send_packet();
         Serial.println("Datagram checksum OK");
       }
     else
       Serial.println("Datagram checksum ERROR");
     
     Serial.flush();
-  }   
+  } 
+  if (send_deadline < millis()) {
+      Serial.println("Timed out, sending udp packet again cause UDP is UDP..");
+      send_packet();
+      send_deadline = millis() + SEND_PERIOD;
+
+  }
 }
 
 bool checkCRC(const byte buffer_datagram[])
@@ -70,3 +81,13 @@ bool checkCRC(const byte buffer_datagram[])
     return false;
 }
 
+void send_packet() {
+  OSCMessage msgOut("/pole_touch");
+  uint8_t * blob = last_packet;
+  msgOut.add(blob, PACKET_SIZE);
+
+  Udp.beginPacket(broadcast_udp,port_udp);
+  msgOut.send(Udp);
+  Udp.endPacket();
+  msgOut.empty();
+}
